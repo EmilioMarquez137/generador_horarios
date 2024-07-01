41f813982deccd8141fcfdacd8970a5f2db208bf
ValeriaJahzeel <vcastanonh2100@alumno.ipn.mx>
2024-07-01 01:12:28 -0600

    ya

--- CRUD_horario.php
+++ CRUD_horario.php
@@ -1,14 +1,13 @@
 <?php
-// Configuración de la base de datos
-$servername = "tu_servidor";
-$username = "tu_usuario";
-$password = "tu_contraseña";
-$dbname = "mydb"; // Nombre de tu base de datos
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$dbname = "mydb";
 
-// Conexión a la base de datos
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar la conexión
+
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
